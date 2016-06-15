db4f626227d458476d2d84823844fab7eda215bb
Alexandr Mikula <mikula@fzu.cz>
2016-06-15 09:53:20 +0200

    Add keywords variable to module managed packages
    (cherry-picked from 55653bc4d428fdbdae854a934fa505c9b65c1493)
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,7 @@ class portage::install {
   portage::package { 'sys-apps/portage':
     ensure   => $portage::portage_ensure,
     keywords => $portage::portage_keywords,
+    keywords_version => $portage::portage_keywords_version,
     use      => $portage::portage_use,
     target   => 'portage',
   }
@@ -12,6 +13,7 @@ class portage::install {
   portage::package { 'app-portage/eix':
     ensure   => $portage::eix_ensure,
     keywords => $portage::eix_keywords,
+    keywords_version => $portage::eix_keywords_version,
     use      => $portage::eix_use,
     target   => 'portage',
   }
@@ -19,6 +21,7 @@ class portage::install {
   portage::package { 'app-portage/layman':
     ensure   => $portage::layman_ensure,
     keywords => $portage::layman_keywords,
+    keywords_version => $portage::layman_keywords_version,
     use      => $portage::layman_use,
     target   => 'portage',
     notify   => Portage::Makeconf["source ${portage::layman_make_conf}"],
@@ -37,6 +40,7 @@ class portage::install {
   portage::package { 'app-admin/webapp-config':
     ensure   => $portage::webapp_config_ensure,
     keywords => $portage::webapp_config_keywords,
+    keywords_version => $portage::webapp_config_keywords_version,
     use      => $portage::webapp_config_use,
     target   => 'portage',
   }
@@ -44,6 +48,7 @@ class portage::install {
   portage::package { 'app-admin/eselect':
     ensure   => $portage::eselect_ensure,
     keywords => $portage::eselect_keywords,
+    keywords_version => $portage::eselect_keywords_version,
     use      => $portage::eselect_use,
     target   => 'portage',
   }
@@ -51,6 +56,7 @@ class portage::install {
   portage::package { 'app-portage/portage-utils':
     ensure   => $portage::portage_utils_ensure,
     keywords => $portage::portage_utils_keywords,
+    keywords_version => $portage::portage_utils_keywords_version,
     use      => $portage::portage_utils_use,
     target   => 'portage',
   }
