2b0114638ed22bacf3ebb6e158d925e8b43daac1
Robin H. Johnson <robbat2@gentoo.org>
2016-12-12 20:20:30 -0800

    fix whitespace.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,28 +3,28 @@ class portage::install {
   include ::portage
 
   portage::package { 'sys-apps/portage':
-    ensure   => $portage::portage_ensure,
-    keywords => $portage::portage_keywords,
+    ensure           => $portage::portage_ensure,
+    keywords         => $portage::portage_keywords,
     keywords_version => $portage::portage_keywords_version,
-    use      => $portage::portage_use,
-    target   => 'portage',
+    use              => $portage::portage_use,
+    target           => 'portage',
   }
 
   portage::package { 'app-portage/eix':
-    ensure   => $portage::eix_ensure,
-    keywords => $portage::eix_keywords,
+    ensure           => $portage::eix_ensure,
+    keywords         => $portage::eix_keywords,
     keywords_version => $portage::eix_keywords_version,
-    use      => $portage::eix_use,
-    target   => 'portage',
+    use              => $portage::eix_use,
+    target           => 'portage',
   }
 
   portage::package { 'app-portage/layman':
-    ensure   => $portage::layman_ensure,
-    keywords => $portage::layman_keywords,
+    ensure           => $portage::layman_ensure,
+    keywords         => $portage::layman_keywords,
     keywords_version => $portage::layman_keywords_version,
-    use      => $portage::layman_use,
-    target   => 'portage',
-    notify   => Portage::Makeconf["source ${portage::layman_make_conf}"],
+    use              => $portage::layman_use,
+    target           => 'portage',
+    notify           => Portage::Makeconf["source ${portage::layman_make_conf}"],
   }
 
   $layman_makeconf_ensure = $portage::layman_ensure ? {
@@ -38,27 +38,27 @@ class portage::install {
   }
 
   portage::package { 'app-admin/webapp-config':
-    ensure   => $portage::webapp_config_ensure,
-    keywords => $portage::webapp_config_keywords,
+    ensure           => $portage::webapp_config_ensure,
+    keywords         => $portage::webapp_config_keywords,
     keywords_version => $portage::webapp_config_keywords_version,
-    use      => $portage::webapp_config_use,
-    target   => 'portage',
+    use              => $portage::webapp_config_use,
+    target           => 'portage',
   }
 
   portage::package { 'app-admin/eselect':
-    ensure   => $portage::eselect_ensure,
-    keywords => $portage::eselect_keywords,
+    ensure           => $portage::eselect_ensure,
+    keywords         => $portage::eselect_keywords,
     keywords_version => $portage::eselect_keywords_version,
-    use      => $portage::eselect_use,
-    target   => 'portage',
+    use              => $portage::eselect_use,
+    target           => 'portage',
   }
 
   portage::package { 'app-portage/portage-utils':
-    ensure   => $portage::portage_utils_ensure,
-    keywords => $portage::portage_utils_keywords,
+    ensure           => $portage::portage_utils_ensure,
+    keywords         => $portage::portage_utils_keywords,
     keywords_version => $portage::portage_utils_keywords_version,
-    use      => $portage::portage_utils_use,
-    target   => 'portage',
+    use              => $portage::portage_utils_use,
+    target           => 'portage',
   }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,31 +7,31 @@
 # This class does not need to be directly included.
 #
 class portage::params {
-  $make_conf              = '/etc/portage/make.conf'
-  $make_conf_remerge      = true
-  $portage_ensure         = installed
-  $portage_keywords       = undef
-  $portage_keywords_version	= undef
-  $portage_use            = undef
-  $eix_ensure             = installed
-  $eix_keywords           = undef
-  $eix_keywords_version	  = undef
-  $eix_use                = undef
-  $layman_ensure          = absent
-  $layman_keywords        = undef
-  $layman_keywords_version = undef
-  $layman_use             = undef
-  $layman_make_conf       = '/var/lib/layman/make.conf'
-  $webapp_config_ensure   = absent
-  $webapp_config_keywords = undef
+  $make_conf                      = '/etc/portage/make.conf'
+  $make_conf_remerge              = true
+  $portage_ensure                 = installed
+  $portage_keywords               = undef
+  $portage_keywords_version       = undef
+  $portage_use                    = undef
+  $eix_ensure                     = installed
+  $eix_keywords                   = undef
+  $eix_keywords_version           = undef
+  $eix_use                        = undef
+  $layman_ensure                  = absent
+  $layman_keywords                = undef
+  $layman_keywords_version        = undef
+  $layman_use                     = undef
+  $layman_make_conf               = '/var/lib/layman/make.conf'
+  $webapp_config_ensure           = absent
+  $webapp_config_keywords         = undef
   $webapp_config_keywords_version = undef
-  $webapp_config_use      = undef
-  $eselect_ensure         = absent
-  $eselect_keywords       = undef
-  $eselect_keywords_version = undef
-  $eselect_use            = undef
-  $portage_utils_ensure   = installed
-  $portage_utils_keywords = undef
+  $webapp_config_use              = undef
+  $eselect_ensure                 = absent
+  $eselect_keywords               = undef
+  $eselect_keywords_version       = undef
+  $eselect_use                    = undef
+  $portage_utils_ensure           = installed
+  $portage_utils_keywords         = undef
   $portage_utils_keywords_version = undef
-  $portage_utils_use      = undef
+  $portage_utils_use              = undef
 }
